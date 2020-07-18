4b5a2471468491aee90302a25d6cb90e340b3000
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-07-18 00:22:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ function deepestChild(){
 function increaseRankBy(n){
   var internal = document.querySelectorAll(".ranked-list li");
   // querySelectorAll returns a "NodeList", not an array.
-  internal.forEach((element.innerHTML) => {
-    //element = parseInt(element) + n;
+  internal.forEach((element) => {
+    element.HTML = parseInt(element.HTML) + n;
   })
 }
